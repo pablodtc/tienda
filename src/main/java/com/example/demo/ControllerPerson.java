daab199f70878b619ecffc34ada3dd17d2deef6a
pdtc <pablo.trinidadcarhuas@gmail.com>
2022-01-08 21:17:14 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/ControllerPerson.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ControllerPerson {
+
+  private final ModelPerson modelPerson;
+
+  @GetMapping("api/name/{name}/{age}")
+  public ModelPerson getName(@PathVariable String name, @PathVariable int age){
+    modelPerson.setName(name);
+    modelPerson.setAge(age);
+    return modelPerson;
+  }
+}
---
+++ src/main/java/com/example/demo/ModelPerson.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelPerson {
+
+  private String name;
+  private int age;
+
+}
